b5fa31bbc16290de4533d37f36dc73e3a3e71054
mysqlboy <eroomydna@gmail.com>
2015-01-22 14:07:51 +0000

    initial commit

---
+++ audit_parser.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+import json
+import os.path
+from optparse import OptionParser
+
+def parse_options():
+    """
+    Parse options from the command line
+    """
+    parser = OptionParser()
+    parser.add_option("-f", "--file", dest="filename")
+
+    #(options, args) = parser.parse_args()
+
+    return parser.parse_args()
+
+
+def read_file(auditname):
+    """
+    Open and read the audit file
+    """
+    if os.path.exists(auditname):
+        try:
+            jdata = open(auditname,'r')
+        except IOerror, e :
+            print "Unable to open file: %s. %s" % (auditname, e) 
+    
+    return jdata
+
+def parseAudit(auditFile):
+    jdata = read_file(auditFile)
+    for i in jdata:
+        json_decoded = json.loads(i)
+        audit_record = json_decoded['audit_record']
+        try:
+            audit_user = ('%s') % (audit_record["priv_user"])
+            audit_dt = audit_record["timestamp"]
+            print len(audit_user)
+            if len(audit_user) != 0 and audit_user not in user:
+             user.append((audit_dt, audit_user))
+        except:
+            pass
+
+def getUniqueUsers(users):
+        uniq = []
+        for i in user:
+                user_name = i[1]
+                uniq.append(user_name)
+
+        myset = set(uniq)
+        for i in myset:
+                print i
+
+def main():
+        global options
+        global user=[]
+
+        audit_file = options.filename
+
+        parseAudit(audit_file)
+
+        print "=================="
+        print "uniq list of users"
+        print "=================="
+        getUniqueUsers(user)
+
+if __name__ == '__main__':
+        main()
